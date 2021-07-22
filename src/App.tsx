1d2bb7d16fa9929678815eb30067678b604ddd77
Robson <robccorreia@gmail.com>
2021-07-22 09:56:55 -0300

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { GlobalStyle } from "./assets/styles/global";
+
 
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyle/>
     </div>
   );
 }
