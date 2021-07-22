9ea8db5acd326dfdd84b729cd8f36cc926d5c58d
Robson <robccorreia@gmail.com>
2021-07-22 14:54:25 -0300

    add mirageJS and axios on project

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+
+
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    api.get('transactions')
+    .then(response => console.log(response.data));
+  }, []);
+
+
   return (
     <Container>
 
